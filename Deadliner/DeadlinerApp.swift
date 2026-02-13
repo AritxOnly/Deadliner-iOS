bfa3f1340f61a1afb25708fc7a0894fe58558cc6
AritxOnly <aritx_zhou@qq.com>
2026-02-13 21:41:32 +0800

    Initial Commit

---
+++ Deadliner/DeadlinerApp.swift
@@ -0,0 +1,32 @@
+//
+//  DeadlinerApp.swift
+//  Deadliner
+//
+//  Created by Aritx 音唯 on 2026/2/13.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DeadlinerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DeadlinerTests/DeadlinerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeadlinerTests.swift
+//  DeadlinerTests
+//
+//  Created by Aritx 音唯 on 2026/2/13.
+//
+
+import Testing
+@testable import Deadliner
+
+struct DeadlinerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
